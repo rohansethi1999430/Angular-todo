e1150bcc4e613c9a3c7fb7cee145781e299616c4
Rohan <rohansethi1999430@gmail.com>
2021-06-19 18:28:53 +0530

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title="Todos-Lists";
+    // }, 2000);
+  }
 }
